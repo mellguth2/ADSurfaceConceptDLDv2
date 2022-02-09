b9f66ddbcfeba0cbb31b8debd59b659584a6086d
Martin Ellguth <ellguth@surface-concept.de>
2022-02-09 18:27:54 +0100

    * work in progress

--- SCDLDApp/Db/generate_db_template/generate_db_template.py
+++ SCDLDApp/Db/generate_db_template/generate_db_template.py
@@ -6,35 +6,50 @@
 INFILE="../../params/parameters.json"
 OUTFILE="../_generated.template"
 import json
+from template_parts import TEMPLATE_START, db_gen_enum, db_gen_int32, \
+  db_gen_float64, db_gen_string
+
+db_gen_fdict = {
+  'enum'    : db_gen_enum,
+  'int32'   : db_gen_int32,
+  'float64' : db_gen_float64,
+  'string'  : db_gen_string }
 
 def generate_template(infile, outfile):
   with open(infile, "r") as f_in:
     parameters = json.load(f_in)
     with open(outfile, "w") as f_out:
+      write = f_out.write
       # ----------
-      #f_out.write(code1)
+      write(TEMPLATE_START)
       # ----------
-      pidx = 0
       for param in parameters:
         if param['node'] != 'parameter':
           continue
-        if not param['read-only']:
-          pass
-          #f_out.write(reg_wr[param['data type']](pidx, param['name']))
-        #f_out.write(reg_rd[param['data type']](pidx, param['name']))
-        pidx += 1
-      # -----------
-      #f_out.write(code2)
-      # -----------
-      pidx = 0
-      for param in parameters:
-        if param['node'] != 'parameter':
-          continue
-        #f_out.write(upd[param['data type']](pidx, param['name']))
-        pidx += 1
-      # -----------
-      #f_out.write(code3)
-
+        datatype = param['data type']
+        f = db_gen_fdict[datatype] # selects the db record string generator
+        # first two positional arguments are common across all db_gen_xxx(...):
+        f_args = [param['name'], param['epicsprops']['asynportname']]
+        # common keyword arguments:
+        f_kwargs = {'defaultval'  : param['default'],
+                    'description' : param['description']}
+        ### add specialties of individual data types
+        if datatype == 'enum':
+          f_kwargs['options'] = param['options']
+        elif datatype == 'int32':
+          pass # no specialties so far
+        elif datatype == 'float64':
+          f_kwargs['precision'] = param['precision']
+          f_kwargs['unit'] = param['unit']
+        elif datatype == 'string':
+          f_kwargs['maxlength'] = param['maxlen']
+        # create a write parameter and a read-back parameter if not read-only:
+        rwlist = (False,) if param['read-only'] else (False, True)
+        nameextlist = ('',) if param['read-only'] else ('_RBV', '')
+        for outval, nameext in (rwlist, nameextlist):
+          f_args[0] = param['name'] + nameextlist
+          f_kwargs['out'] = outval
+          write(f(*f_args, **f_kwargs))
 
 if __name__ == "__main__":
   generate_template(INFILE, OUTFILE)
