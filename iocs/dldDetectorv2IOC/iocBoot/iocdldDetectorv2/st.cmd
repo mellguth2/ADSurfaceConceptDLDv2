7bb1321cd1022ca1e6f3659c59c1d8d5cb9b0cf1
Martin Ellguth <ellguth@surface-concept.de>
2022-04-28 11:56:13 +0200

    * synchronized iocBoot

--- iocs/dldDetectorv2IOC/iocBoot/iocdldDetectorv2/st.cmd
+++ iocs/dldDetectorv2IOC/iocBoot/iocdldDetectorv2/st.cmd
@@ -2,6 +2,8 @@
 
 errlogInit(20000)
 
+epicsEnvSet("ADCORE","$(EPICS_HOME)/synApps_5_8/support/areaDetector/ADCore")
+
 dbLoadDatabase("$(EPICS_HOME)/install/dbd/dldDetectorv2App.dbd")
 dldDetectorv2App_registerRecordDeviceDriver(pdbbase) 
 
@@ -24,37 +26,28 @@ epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(EPICS_HOME)/install/db")
 
 # asynSetMinTimerPeriod(0.001) # this causes an error
 
-# The EPICS environment variable EPICS_CA_MAX_ARRAY_BYTES needs to be set to a value at least as large
-# as the largest image that the standard arrays plugin will send.
-# That vlaue is $(XSIZE) * $(YSIZE) * sizeof(FTVL data type) for the FTVL used when loading the NDStdArrays.template file.
-# The variable can be set in the environment before running the IOC or it can be set here.
-# It is often convenient to set it in the environment outside the IOC to the largest array any client 
-# or server will need.  For example 10000000 (ten million) bytes may be enough.
-# If it is set here then remember to also set it outside the IOC for any CA clients that need to access the waveform record.  
-# Do not set EPICS_CA_MAX_ARRAY_BYTES to a value much larger than that required, because EPICS Channel Access actually
-# allocates arrays of this size every time it needs a buffer larger than 16K.
-# Uncomment the following line to set it in the IOC.
-#epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES", "10000000")
-
-# dldDetectorv2Config(const char *portName, int maxSizeX, int maxSizeY, int dataType,
-#                   int maxBuffers, int maxMemory, int priority, int stackSize)
-dldDetectorv2Config("$(PORT)", $(XSIZE), $(YSIZE), 1, 0, 0)
+### dldDetectorv2Config(const char *portName, int maxSizeX, int maxSizeY, int dataType,
+###                   int maxBuffers, int maxMemory, int priority, int stackSize)
+dldDetectorv2Config("$(PORT)", $(XSIZE), $(YSIZE), 4, 0, 0)
 # To have the rate calculation use a non-zero smoothing factor use the following line
 #dbLoadRecords("dldDetectorv2.template",     "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1,RATE_SMOOTH=0.2")
 dbLoadRecords("$(EPICS_HOME)/install/db/dldDetectorv2.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
 
 # Create a standard arrays plugin, set it to get data from detector driver.
+# NDStdArraysConfigure(portName, frame queue size, blocking callbacks, NDArrayPort, NDArrayAddr, maxMemory, priority, stacksize)
 NDStdArraysConfigure("Image1", 3, 0, "$(PORT)", 0)
 
 # This creates a waveform large enough for 2000x2000x3 (e.g. RGB color) arrays.
 # This waveform allows transporting 32-bit images
-dbLoadRecords("NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),TYPE=Int32,FTVL=LONG,NELEMENTS=12000000")
+dbLoadRecords("NDStdArrays.template", "P=$(PREFIX),R=LiveImageXY:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),TYPE=Int32,FTVL=LONG,NELEMENTS=12000000")
 
 
 # Load all other plugins using commonPlugins.cmd
-# < $(ADCORE)/iocBoot/commonPlugins.cmd
+< commonPlugins.cmd
 set_requestfile_path("$(EPICS_HOME)/install/req")
-
+set_savefile_path("$(EPICS_HOME)/iocBoot/iocdldDetectorv2/autosave")
+set_pass0_restoreFile("auto_settings.sav")
+set_pass1_restoreFile("auto_settings.sav")
 
 iocInit()
 
